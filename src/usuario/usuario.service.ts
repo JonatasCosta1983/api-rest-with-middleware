f5f7c2591cff24bd4d3499bed56c6e9d95aba8da
JonatasCosta1983 <jonathanevescosta@gmail.com>
2023-05-21 17:01:03 -0300

    integrated the prism orm to the database

--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -2,31 +2,45 @@ import { Injectable } from '@nestjs/common';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 import { ParcialUsuarioDto } from './dto/parcial-usuario.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsuarioService {
+  constructor(private readonly prisma: PrismaService) {}
   
   async create(data: CreateUsuarioDto) {
-    return {data}
-  }
-
-  async findAll() {
-    return {usuarios:[]};
+    return this.prisma.usuario.create({
+      data: data
+    })
   }
 
   async findOne(id: number) {
-    return {id}
+    return this.prisma.usuario.findUnique({
+      where:{id}
+    })
+  }
+
+  async findAll() {
+    return this.prisma.usuario.findMany()
   }
 
   async update(id: number, data: UpdateUsuarioDto) {
-    return {id, data}
+    return this.prisma.usuario.update({
+      data: data,
+      where:{id}
+    })
   }
 
   async parcial(id: number, data: ParcialUsuarioDto) {
-    return {id, data}
+    return this.prisma.usuario.update({
+      data: data,
+      where:{id}
+    })
   }
 
   async remove(id: number) {
-    return {id}
+    return this.prisma.usuario.delete({
+      where:{id}
+    })
   }
 }
